20f9518be6af4b5b6cdf6a763fb05929115ff2ff
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-14 00:02:15 +0800

    EggTimer :-)

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time = 0
+    
+    var timer = NSTimer()
+    
+    @IBOutlet weak var timerDisplay: UILabel!
+
+    @IBAction func resetTimer(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        timerDisplay.text = "0"
+    }
+    
+    @IBAction func startTimer(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseTimer(sender: AnyObject) {
+        timer.invalidate()
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func result() {
+        time++
+        timerDisplay.text = String(time)
+    }
 
 }
 
